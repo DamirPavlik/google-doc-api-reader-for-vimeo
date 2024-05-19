951a890c9ff1dea219d23c6435b6e1a44f374d4b
DamirPavlik <damir.pavlik@gmail.com>
2024-05-19 13:13:42 +0200

    feat: added login button

---
+++ src/components/loginButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login'
+
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
+const loginButton = () => {
+    const onSuccess = (res) => {
+        console.log("Login success! Current user: ", res.profileObj);
+    }
+
+    const onFailure = () => {
+        console.log("Login Failed! res: ", res);
+    }
+
+    return (
+    <div id='singInButton'>
+        <GoogleLogin
+            clientId={clientId}
+            buttonText='Login'
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            isSignedIn={true}
+        />
+    </div>
+  )
+}
+
+export default loginButton
