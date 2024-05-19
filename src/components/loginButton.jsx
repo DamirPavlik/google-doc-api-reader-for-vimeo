1fe12fae28d0da5e3d3d13c32f1643871a4e2fe8
DamirPavlik <damir.pavlik@gmail.com>
2024-05-20 00:51:06 +0200

    feat: added status context from log in button

--- src/components/loginButton.jsx
+++ src/components/loginButton.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import GoogleLogin from 'react-google-login'
+import StatusContext from '../context/statusProvider';
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
 
 const loginButton = () => {
+    const { setStatus } = useContext(StatusContext);
+
     const onSuccess = (res) => {
         console.log("Login success! Current user: ", res.profileObj);
-    }
+        setStatus("Logged In");
+    };
 
-    const onFailure = () => {
+    const onFailure = (res) => {
         console.log("Login Failed! res: ", res);
-    }
+        setStatus("Logged out");
+    };
 
     return (
     <div id='singInButton' className='mr-10'>
